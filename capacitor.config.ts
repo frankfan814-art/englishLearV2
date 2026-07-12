57f4d4c85062426d0cd1941a37670aeb1ccb099a
fanhailiang <hailiang.fan@outlook.com>
2026-07-12 10:03:33 +0800

    build: initialize capacitor and android project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vocab.app',
+  appName: '单词朗读',
+  webDir: 'dist'
+};
+
+export default config;
